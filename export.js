81b41ee47a649c43591d9cc5a84817ad4ec73552
ashtonk718 <82291600+ashtonk718@users.noreply.github.com>
2023-12-27 13:34:01 -0600

    Update export.js

--- export.js
+++ export.js
@@ -1,7 +1,7 @@
 // file2.js
 
 // Import the variable from file1.js
-import { myVariable } from 'import.js';
+import { myVariable } from './import.js';
 
 // Log the imported variable
 console.log(myVariable);
